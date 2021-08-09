7fd12ccb08edf4ba1dc2627bb775cc73feddd1c8
fabiosandernt <fabiosandernt@gmail.com>
2021-08-09 15:13:20 -0300

    comitando

---
+++ ListandoArquivos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ListandoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Marca o diretório a ser listado
+            DirectoryInfo diretorio = new DirectoryInfo(@"C:\Projects\MOD01-EnvioDeE-mail\fila\");
+            //Executa função GetFile(Lista os arquivos desejados de acordo com o parametro)
+            FileInfo[] Arquivos = diretorio.GetFiles("*.*");
+
+            //Começamos a listar os arquivos
+            foreach (FileInfo fileinfo in Arquivos)
+            {
+                Console.WriteLine(fileinfo.Name); 
+                //Response.Write(fileinfo.Name);
+            }
+            
+        }
+    }
+}
